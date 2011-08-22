e76cf8d58e26f0a8d7656f1c165d34f660f237d8
ThomasVolkert <silvo@gmx.net>
2011-08-22 13:48:41 +0000

    - first version on SF

---
+++ HomerSoundOutput/include/WaveOut.h
@@ -0,0 +1,62 @@
+/*
+ * Name:    WaveOut.h
+ * Purpose: abstract wave out
+ * Author:  Thomas Volkert
+ * Since:   2010-11-30
+ * Version: $Id: WaveOut.h 11 2011-08-22 13:15:28Z silvo $
+ */
+
+#ifndef _SOUNDOUT_WAVE_OUT_
+#define _SOUNDOUT_WAVE_OUT_
+
+#include <MediaSource.h>
+#include <PacketStatistic.h>
+
+namespace Homer { namespace SoundOutput {
+
+///////////////////////////////////////////////////////////////////////////////
+
+class WaveOut:
+    public Homer::Monitor::PacketStatistic
+{
+public:
+    WaveOut(std::string pName = "");
+
+    /// The destructor
+    virtual ~WaveOut();
+
+    /* device interface */
+    virtual bool SelectDevice(std::string pDeviceName = "");
+
+    // playback control
+    virtual void StopPlayback();
+
+public:
+    /* abstract interface which has to be implemented by derived classes */
+
+    /* open/close */
+    virtual bool OpenWaveOutDevice(int pSampleRate = 44100, bool pStereo = true) = 0;
+    virtual bool CloseWaveOutDevice() = 0;
+    /* device interface */
+    virtual void getAudioDevices(Homer::Multimedia::AudioDevicesList &pAList) = 0;
+    /* playback control */
+    virtual bool WriteChunk(void* pChunkBuffer, int pChunkSize = 4096) = 0;
+
+protected:
+    /* device state */
+    bool                mWaveOutOpened;
+    bool                mPlaybackStopped;
+    int                 mChunkNumber;
+    /* device parameters */
+    int                 mSampleRate;
+    bool                mStereo;
+    /* device handling */
+    std::string         mDesiredDevice;
+    std::string         mCurrentDevice;
+};
+
+///////////////////////////////////////////////////////////////////////////////
+
+}} // namespaces
+
+#endif
---
+++ HomerSoundOutput/include/WaveOutAlsa.h
@@ -0,0 +1,55 @@
+/*
+ * Name:    WaveOutAlsa.h
+ * Purpose: wave out based on ALSA
+ * Author:  Thomas Volkert
+ * Since:   2010-12-11
+ * Version: $Id: WaveOutAlsa.h 11 2011-08-22 13:15:28Z silvo $
+ */
+
+#ifdef LINUX
+#ifndef _SOUNDOUT_WAVE_OUT_ALSA_
+#define _SOUNDOUT_WAVE_OUT_ALSA_
+
+#include <WaveOut.h>
+#include <Header_Alsa.h>
+
+namespace Homer { namespace SoundOutput {
+
+///////////////////////////////////////////////////////////////////////////////
+
+// de/activate debugging of grabbed packets
+//#define WOA_DEBUG_PACKETS
+
+///////////////////////////////////////////////////////////////////////////////
+
+class WaveOutAlsa:
+    public WaveOut
+{
+public:
+    WaveOutAlsa(std::string pDesiredDevice = "");
+
+    /// The destructor
+    virtual ~WaveOutAlsa();
+
+    // playback control
+    virtual void StopPlayback();
+
+public:
+    /* open/close */
+    virtual bool OpenWaveOutDevice(int pSampleRate = 44100, bool pStereo = true);
+    virtual bool CloseWaveOutDevice();
+    /* device interface */
+    virtual void getAudioDevices(Homer::Multimedia::AudioDevicesList &pAList);
+    /* playback control */
+    virtual bool WriteChunk(void* pChunkBuffer, int pChunkSize = 4096);
+
+private:
+    snd_pcm_t           *mPlaybackHandle;
+};
+
+///////////////////////////////////////////////////////////////////////////////
+
+}} // namespaces
+
+#endif
+#endif
