b5f582265adde65796b320117efee846ceb3a9c2
ThomasVolkert <silvo@gmx.net>
2011-08-29 21:14:17 +0000

    - LICENSE

--- HomerMonitor/src/ProcessStatistic.cpp
+++ HomerMonitor/src/ProcessStatistic.cpp
@@ -1,5 +1,26 @@
+/*****************************************************************************
+ *
+ * Copyright (C) 2008-2011 Homer-conferencing project
+ *
+ * This software is free software.
+ * Your are allowed to redistribute it and/or modify it under the terms of
+ * the GNU General Public License version 2 as published by the Free Software
+ * Foundation.
+ *
+ * This source is published in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License version 2 for more details.
+ *
+ * You should have received a copy of the GNU General Public License version 2
+ * along with this program. Otherwise, you can write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02111, USA.
+ * Alternatively, you find an online version of the license text under
+ * http://www.gnu.org/licenses/gpl-2.0.html.
+ *
+ *****************************************************************************/
+
 /*
- * Name:    ProcessStatistic.C
+ * Name:    ProcessStatistic.cpp
  * Purpose: Implemenation of process statistic
  * Author:  Thomas Volkert
  * Since:   2009-05-18
--- HomerMonitor/src/ProcessStatisticService.cpp
+++ HomerMonitor/src/ProcessStatisticService.cpp
@@ -1,3 +1,24 @@
+/*****************************************************************************
+ *
+ * Copyright (C) 2008-2011 Homer-conferencing project
+ *
+ * This software is free software.
+ * Your are allowed to redistribute it and/or modify it under the terms of
+ * the GNU General Public License version 2 as published by the Free Software
+ * Foundation.
+ *
+ * This source is published in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License version 2 for more details.
+ *
+ * You should have received a copy of the GNU General Public License version 2
+ * along with this program. Otherwise, you can write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02111, USA.
+ * Alternatively, you find an online version of the license text under
+ * http://www.gnu.org/licenses/gpl-2.0.html.
+ *
+ *****************************************************************************/
+
 /*
  * Name:    ProcessStatisticService.cpp
  * Purpose: Implemenation of process statistic service as singleton
