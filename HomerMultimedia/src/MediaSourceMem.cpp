14b025f93fa07cf9591bc11ae8da0ab1eea0fd12
ThomasVolkert <silvo@gmx.net>
2012-02-29 23:15:12 +0100

    - extended list of supported video resolutions

--- HomerMultimedia/src/MediaSourceMem.cpp
+++ HomerMultimedia/src/MediaSourceMem.cpp
@@ -227,12 +227,12 @@ GrabResolutions MediaSourceMem::GetSupportedVideoGrabResolutions()
         tFormat.Name="SQCIF";      //      128 ×  96
         tFormat.ResX = 128;
         tFormat.ResY = 96;
-        //mSupportedVideoFormats.push_back(tFormat);
+        mSupportedVideoFormats.push_back(tFormat);
 
         tFormat.Name="QCIF";       //      176 × 144
         tFormat.ResX = 176;
         tFormat.ResY = 144;
-        //mSupportedVideoFormats.push_back(tFormat);
+        mSupportedVideoFormats.push_back(tFormat);
 
         tFormat.Name="CIF";        //      352 × 288
         tFormat.ResX = 352;
@@ -247,7 +247,12 @@ GrabResolutions MediaSourceMem::GetSupportedVideoGrabResolutions()
         tFormat.Name="CIF4";       //      704 × 576
         tFormat.ResX = 704;
         tFormat.ResY = 576;
-        //mSupportedVideoFormats.push_back(tFormat);
+        mSupportedVideoFormats.push_back(tFormat);
+
+        tFormat.Name="DVD";        //      720 × 576
+        tFormat.ResX = 720;
+        tFormat.ResY = 576;
+        mSupportedVideoFormats.push_back(tFormat);
 
         tFormat.Name="SVGA";       //
         tFormat.ResX = 800;
@@ -262,12 +267,27 @@ GrabResolutions MediaSourceMem::GetSupportedVideoGrabResolutions()
         tFormat.Name="CIF9";       //     1056 × 864
         tFormat.ResX = 1056;
         tFormat.ResY = 864;
-        //mSupportedVideoFormats.push_back(tFormat);
+        mSupportedVideoFormats.push_back(tFormat);
+
+        tFormat.Name="SXGA";       //     1280 × 1024
+        tFormat.ResX = 1280;
+        tFormat.ResY = 1024;
+        mSupportedVideoFormats.push_back(tFormat);
+
+        tFormat.Name="WXGA+";      //     1440 × 900
+        tFormat.ResX = 1440;
+        tFormat.ResY = 900;
+        mSupportedVideoFormats.push_back(tFormat);
+
+        tFormat.Name="SXGA+";       //     1440 × 1050
+        tFormat.ResX = 1440;
+        tFormat.ResY = 1050;
+        mSupportedVideoFormats.push_back(tFormat);
 
         tFormat.Name="CIF16";      //     1408 × 1152
         tFormat.ResX = 1408;
         tFormat.ResY = 1152;
-        //mSupportedVideoFormats.push_back(tFormat);
+        mSupportedVideoFormats.push_back(tFormat);
 
         tFormat.Name="HDTV";       //     1920 × 1080
         tFormat.ResX = 1920;
