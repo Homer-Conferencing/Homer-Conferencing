b88ecba6fd5c5e0613c7fd2e5aea0414431ad678
ThomasVolkert <silvo@gmx.net>
2012-04-17 19:43:18 +0200

    - cleanup

--- HomerGAPI/include/IConnection.h
+++ HomerGAPI/include/IConnection.h
@@ -25,8 +25,8 @@
  * Since:   2011-12-08
  */
 
-#ifndef _GAPI_ISUBSCRIPTION_
-#define _GAPI_ISUBSCRIPTION_
+#ifndef _GAPI_ICONNECTION_
+#define _GAPI_ICONNECTION_
 
 #include <Name.h>
 #include <Requirements.h>
