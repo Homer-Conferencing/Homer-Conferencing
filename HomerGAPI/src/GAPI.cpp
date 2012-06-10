661e8c3bbc3bfbceed958b2cd393d086f4694b4b
ThomasVolkert <silvo@gmx.net>
2012-06-10 16:36:43 +0200

    - removing NGN from master branch - cleanup

--- HomerGAPI/src/GAPI.cpp
+++ HomerGAPI/src/GAPI.cpp
@@ -27,7 +27,6 @@
 
 #include <GAPI.h>
 #include <Berkeley/SocketSetup.h>
-#include <NextGenNet/NGNSocketSetup.h>
 
 #include <Logger.h>
 
@@ -46,7 +45,6 @@ GapiService::GapiService()
 	mSetupInterface = NULL;
 	mSetupInterfaceName = "";
 	registerImpl(new SocketSetup(), BERKEYLEY_SOCKETS);
-	registerImpl(new NGNSocketSetup(), NGN_SOCKETS);
 }
 
 GapiService::~GapiService()
