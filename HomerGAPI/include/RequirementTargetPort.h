5f9e262f90561216c950f74e86a6026fa8e1c049
ThomasVolkert <silvo@gmx.net>
2012-07-14 00:31:50 +0200

    - cleanup

--- HomerGAPI/include/RequirementTargetPort.h
+++ HomerGAPI/include/RequirementTargetPort.h
@@ -43,7 +43,7 @@ public:
     virtual std::string getDescription(){ return "Requ(TargetPort[" + toString(mPort) + "])"; }
 
     int getPort(){ return mPort; }
-    int setPort(int pPort){ mPort = pPort; }
+    void setPort(int pPort){ mPort = pPort; }
 private:
     int     mPort;
 };
