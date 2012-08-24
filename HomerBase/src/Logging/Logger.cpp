1994a2e6555fe15fe2fe5d0c7780c84c400aca62
ThomasVolkert <silvo@gmx.net>
2012-08-24 19:54:14 +0200

    - set default timeout of logger from 50 ms to 250 ms

--- HomerBase/src/Logging/Logger.cpp
+++ HomerBase/src/Logging/Logger.cpp
@@ -234,7 +234,7 @@ void Logger::AddMessage(int pLevel, const char *pSource, int pLine, const char* 
     tFinalMessage = toString(tMessageBuffer);
 
     // lock
-    if (mLoggerMutex.tryLock(50))
+    if (mLoggerMutex.tryLock(250))
     {
         if ((mLastMessage != tFinalMessage) || (mLastSource != tFinalSource) || (mLastLine != pLine))
         {
