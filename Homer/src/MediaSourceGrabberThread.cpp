8afd4a12ef7ab8c120f8f5dfe8e465531492428d
ThomasVolkert <thomas@homer-conferencing.com>
2013-01-03 20:21:14 +0100

    - fixed: correct EOF detection

--- Homer/src/MediaSourceGrabberThread.cpp
+++ Homer/src/MediaSourceGrabberThread.cpp
@@ -295,7 +295,7 @@ void MediaSourceGrabberThread::StopFile()
 
 bool MediaSourceGrabberThread::EofReached()
 {
-	return (((mEofReached) && (!mResetMediaSourceAsap) && (!mPlayNewFileAsap) && (!mSeekAsap)) || (mPlayNewFileAsap));
+	return ((mEofReached) && (!mResetMediaSourceAsap) && (!mPlayNewFileAsap) && (!mSeekAsap) && (!mSetCurrentDeviceAsap));
 }
 
 QString MediaSourceGrabberThread::CurrentFile()
