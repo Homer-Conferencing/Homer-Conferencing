fcf893df3d54a6777aba02f8aaadd27513e4ca7c
ThomasVolkert <thomas@homer-conferencing.com>
2013-01-15 00:37:18 +0100

    -

--- Homer/src/Widgets/AudioWidget.cpp
+++ Homer/src/Widgets/AudioWidget.cpp
@@ -524,8 +524,11 @@ QStringList AudioWidget::GetAudioStatistic()
 	//### Line 2: current audio buffer, dropped chunks, buffered packets
     QString tLine_Frame = "";
     tLine_Frame = "Frame: " + QString("%1").arg(mCurrentFrameNumber);
-    tLine_Frame += (mAudioSource->GetChunkDropCounter() ? (" (" + QString("%1").arg(mAudioSource->GetChunkDropCounter()) + " " + Homer::Gui::AudioWidget::tr("lost packets")) : "");
-    tLine_Frame += (mAudioSource->GetRelativeLoss() ? (", " + QString("%1").arg(mAudioSource->GetRelativeLoss(), 2, 'f', 2, (QLatin1Char)' ') + Homer::Gui::AudioWidget::tr("% loss")) + ")" : ")");
+    if (mAudioSource->GetChunkDropCounter())
+    {
+        tLine_Frame += " (" + QString("%1").arg(mAudioSource->GetChunkDropCounter()) + " " + Homer::Gui::AudioWidget::tr("lost packets");
+        tLine_Frame += (mAudioSource->GetRelativeLoss() ? (", " + QString("%1").arg(mAudioSource->GetRelativeLoss(), 2, 'f', 2, (QLatin1Char)' ') + Homer::Gui::AudioWidget::tr("% loss")) + ")" : ")");
+    }
     tLine_Frame += (mAudioSource->GetFragmentBufferCounter() ? (" (" + QString("%1").arg(mAudioSource->GetFragmentBufferCounter()) + "/" + QString("%1").arg(mAudioSource->GetFragmentBufferSize()) + " " + Homer::Gui::AudioWidget::tr("buffered packets") + ")") : "");
 
     //############################################
