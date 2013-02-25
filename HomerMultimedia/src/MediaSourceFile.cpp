f435ed25f831ba2b8cbc0a33284142f20c229e41
ThomasVolkert <thomas@homer-conferencing.com>
2013-02-25 18:28:45 +0100

    - fixed: increased buffering for 32 bit systems

--- HomerMultimedia/src/MediaSourceFile.cpp
+++ HomerMultimedia/src/MediaSourceFile.cpp
@@ -53,7 +53,7 @@ using namespace Homer::Monitor;
 #define MSF_FRAME_INPUT_QUEUE_MAX_TIME_WEB_STREAMS         ((System::GetTargetMachineType() != "x86") ? 3.0 : 3.0)
 
 // how much time do we want to buffer at maximum?
-#define MSF_FRAME_INPUT_QUEUE_MAX_TIME                     ((System::GetTargetMachineType() != "x86") ? 3.0 : 0.5) // 0.5 seconds for 32 bit targets with limit of 4 GB ram, 3.0 seconds for 64 bit targets
+#define MSF_FRAME_INPUT_QUEUE_MAX_TIME                     ((System::GetTargetMachineType() != "x86") ? 3.0 : 1.5) // 1.5 seconds for 32 bit targets with limit of 4 GB ram, 3.0 seconds for 64 bit targets
 
 // how long do we want to wait for a next key frame after seeking?
 #define MSF_SEEK_WAITING_FOR_NEXT_KEY_FRAME_TIMEOUT         3 // seconds
