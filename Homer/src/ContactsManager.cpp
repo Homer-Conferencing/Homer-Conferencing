695ef173d024cd637d54d7afdf64308aad86605a
ThomasVolkert <thomas@homer-conferencing.com>
2013-12-06 13:51:29 +0100

    - fixed: detection of unknown contacts

--- Homer/src/ContactsManager.cpp
+++ Homer/src/ContactsManager.cpp
@@ -506,19 +506,22 @@ void ContactsManager::UpdateContact(QString pContact, enum TransportType pContac
     // add the unknown contact
     if(!tFoundContact)
     {
-        if(CONF.GetSipUnknownContactsProbing())
+        if(pState)
         {
-            LOG(LOG_VERBOSE, "Detected unknown contact: %s[%s] with software %s", pContact.toStdString().c_str(), tContactTransport.toStdString().c_str(), pSoftware.toStdString().c_str());
-            ContactDescriptor tUnknownContact;
-            tUnknownContact.User = tUser;
-            tUnknownContact.Host = tHost;
-            tUnknownContact.Port = tPort;
-            tUnknownContact.Name = tUnknownContact.User;
-            tUnknownContact.Transport = pContactTransport;
-            tUnknownContact.Software = pSoftware;
-            tUnknownContact.Unknown = true;
-            tUnknownContact.Id = CONTACTS.GetNextFreeId();
-            AddContact(tUnknownContact);
+            if(CONF.GetSipUnknownContactsProbing())
+            {
+                LOG(LOG_VERBOSE, "Detected unknown contact: %s[%s] with software %s", pContact.toStdString().c_str(), tContactTransport.toStdString().c_str(), pSoftware.toStdString().c_str());
+                ContactDescriptor tUnknownContact;
+                tUnknownContact.User = tUser;
+                tUnknownContact.Host = tHost;
+                tUnknownContact.Port = tPort;
+                tUnknownContact.Name = tUnknownContact.User;
+                tUnknownContact.Transport = pContactTransport;
+                tUnknownContact.Software = pSoftware;
+                tUnknownContact.Unknown = true;
+                tUnknownContact.Id = CONTACTS.GetNextFreeId();
+                AddContact(tUnknownContact);
+            }
         }
     }
 
